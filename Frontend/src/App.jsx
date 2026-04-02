0f87fa066e4dab4ba3d86c9638740f4d8ca03c87
levietngocc <vietngocambition@gmail.com>
2026-04-02 16:56:47 +0700

    feat: add ProviderLayout and GuideManagementProvider components, update routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -19,9 +19,8 @@ import ProviderBookingManagement from "./pages/Provider/ProviderBookingManagemen
 import LandingHome from "./pages/LandingHome";
 import ProviderDashboard from "./pages/Provider/ProviderDashboard";
 import GuideManagementProvider from "./pages/Provider/GuideManagementProvider";
-
-import LandingHome from "./pages/LandingHome";
 import ProviderBookingManagement from "./pages/Provider/ProviderBookingManagement";
+
 
 function AppRoutes() {
   const location = useLocation();
